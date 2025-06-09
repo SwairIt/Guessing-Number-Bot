42da5725595d8e94fc41be083aa01fb14e7a2516
SwairIT <1q2w3e4r5t6y7u89o0pi@gmail.com>
2025-06-09 15:32:36 +0300

    Add banner, update readme.md and starts_bot.sh

--- scripts/starts_bot.sh
+++ scripts/starts_bot.sh
@@ -2,9 +2,13 @@
 
 MAIN="../src/super_bot/main.py"
 
+echo "Check dependencies..."
+poetry check || poetry install
+
 if [ ! -f "$MAIN" ]; then
     echo "Error: Main script not found at $MAIN"
     exit 1
 fi
+
 
 poetry run python3 "$MAIN"
